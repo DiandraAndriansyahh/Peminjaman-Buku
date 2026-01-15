a2e6a73922bce1a9502e0b3697f1db4a30eb5b4d
DiandraAndriansyah <andriansyahdevan@gmail.com>
2026-01-15 13:20:20 +0700

    Menambahkan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return 'Dashboard';
+})->middleware('auth');
 
 Route::get('/', function () {
     return view('welcome');
